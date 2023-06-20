3153463dbfd1473b71a0335de1c0eefde42e7f36
Sehyo Chang <sehyo@fluvio.io>
2023-06-19 18:57:13 -0700

    fix bin path

--- actions/fluvio-install.sh
+++ actions/fluvio-install.sh
@@ -6,5 +6,3 @@ set -eu -o pipefail
 echo "Installing Fluvio Local Cluster"
 
 curl -fsS https://packages.fluvio.io/v1/install.sh | bash
-echo 'export PATH="$HOME/.fluvio/bin:$PATH"' >> $HOME/.bash_profile
-. $HOME/.bash_profile
