3fb421d9db79e94167c25e38ea8a695891d9c53a
Luis Moreno <morenol@users.noreply.github.com>
2023-06-23 12:09:59 -0400

    chore: don't log URL

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use config::DuckDBConfig;
 use fluvio_connector_common::{
     connector,
     consumer::ConsumerStream,
-    tracing::{info, trace},
+    tracing::{debug, info, trace},
     Result, Sink,
 };
 use fluvio_model_sql::Operation;
@@ -18,8 +18,9 @@ use sink::DuckDBSink;
 async fn start(config: DuckDBConfig, mut stream: impl ConsumerStream) -> Result<()> {
     let sink = DuckDBSink::new(&config)?;
     let mut sink = sink.connect(None).await?;
-    info!("connected to duckdb database");
+    info!("Starting consumer loop");
     while let Some(item) = stream.next().await {
+        debug!("received record");
         let operation: Operation = serde_json::from_slice(item?.as_ref())?;
         trace!(?operation);
         sink.send(operation).await?;
